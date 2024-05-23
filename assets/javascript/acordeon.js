9e1b8ff7d941a7beb1ab908103a8086e5447bfc6
Isabelle0210 <isabelleisa646@gmail.com>
2024-05-23 17:13:08 -0300

    mudanças

--- assets/javascript/acordeon.js
+++ assets/javascript/acordeon.js
@@ -1,4 +1,22 @@
-const acordeonTrigger =document.querySelectorAll('.acordeon .acordeon__btn')
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('.title');
+    const text = title.textContent;
+    title.innerHTML = text.split('').map(letter => `<span>${letter}</span>`).join('');
+});
+//aqui estou colocando um evento em que as minhas letras de nav__title ficam se mexendo
+
+window.onload = () => {
+    var dynamicText = document.getElementById('dynamicText');
+    var color = ['pink', 'lightblue', 'lightgreen','ligthpurple'];
+    var currentIndex=0;
+
+    setInterval(function(){
+        dynamicText.style.color = color[currentIndex];
+        currentIndex = (currentIndex + 1) % color.length;
+    }, 1000);
+}
+
+/*const acordeonTrigger =document.querySelectorAll('.acordeon .acordeon__btn')
 
 acordeonTrigger.forEach((acordeon__btn)=>{
     acordeon__btn.addEventListener('click',(e)=>{
@@ -11,3 +29,4 @@ acordeonTrigger.forEach((acordeon__btn)=>{
         }
     })
 })
+*/
